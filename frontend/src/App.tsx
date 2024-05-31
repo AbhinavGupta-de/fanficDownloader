86423fe00f654becc2b34798ba0502add6536405
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-05-31 14:17:43 +0530

    feat: added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import Download from './main/Download';
+import Footer from './main/Footer';
 import Header from './main/Header';
 import Main from './main/Main';
 
 const App = () => {
 	return (
-		<div className="w-[350px] h-[500px] bg-background font-body">
+		<div className="w-[350px] h-[400px] bg-background font-body border-2 border-white">
 			<Header />
 			<Main />
+			<Download />
+			<Footer />
 		</div>
 	);
 };
