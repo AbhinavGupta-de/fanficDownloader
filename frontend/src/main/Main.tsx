c54c11c0912107709f0685fc894c91043195b4f3
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-05-31 14:28:01 +0530

    feat: added download buttons

--- frontend/src/main/Main.tsx
+++ frontend/src/main/Main.tsx
@@ -1,11 +1,11 @@
 const Main = () => {
-	const title = document.querySelector('title');
+	const title = document.querySelector('title heading');
 	const author = document.querySelector('byline heading');
 	return (
-		<div>
-			<div>
-				<div>{title ? title.textContent : 'Invalid '}</div>
-				<div>{author ? author.textContent : 'Invalid '}</div>
+		<div className="flex p-5">
+			<div className="flex flex-col">
+				<div className="text-white">{title ? title.textContent : 'Invalid '}</div>
+				<div className="text-white">{author ? author.textContent : 'Invalid '}</div>
 			</div>
 		</div>
 	);
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,8 @@ export default {
 			primary: '#FF463F',
 			background: '#20231F',
 			white: '#FFFFFF',
+			secondary: '#FF8580',
+			backgroundSecondary: '#85EE83',
 		},
 		extend: {
 			fontFamily: {
