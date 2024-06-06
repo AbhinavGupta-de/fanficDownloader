852fdd7f5454ec6bd4a850c9bdcd6cf77397eaa8
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-06-06 17:23:59 +0530

    fix: removed payload, test with body

--- server/singleChapterFunc/index.js
+++ server/singleChapterFunc/index.js
@@ -16,48 +16,49 @@ async function getChapterContent(url, log) {
 export default async ({ req, res, log }) => {
 	try {
 		log('Function invoked'); // Log the function invocation
-		log(req);
-		log(`Request payload: ${req.payload}`);
+		// log(req);
+		// log(`Request payload: ${req.payload}`);
 
-		const payload = JSON.parse(req.payload);
-		log(`Parsed payload: ${JSON.stringify(payload)}`);
+		log(req.body);
+		// const payload = JSON.parse(req.payload);
+		// log(`Parsed payload: ${JSON.stringify(payload)}`);
 
-		const { url } = payload;
-		if (!url) {
-			log('No URL provided');
-			return res.json({ error: 'URL is required' }, 400);
-		}
+		// 	const { url } = req.body;
+		// 	if (!url) {
+		// 		log('No URL provided');
+		// 		return res.json({ error: 'URL is required' }, 400);
+		// 	}
 
-		log(`Fetching content from URL: ${url}`);
-		const chapterContent = await getChapterContent(url, log);
-		log('Content fetched successfully');
+		// 	log(`Fetching content from URL: ${url}`);
+		// 	const chapterContent = await getChapterContent(url, log);
+		// 	log('Content fetched successfully');
 
-		const browser = await puppeteer.launch({
-			args: ['--no-sandbox', '--disable-setuid-sandbox'],
-		});
-		const page = await browser.newPage();
-		await page.setContent(chapterContent);
-		log('Content set successfully');
+		// 	const browser = await puppeteer.launch({
+		// 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+		// 	});
+		// 	const page = await browser.newPage();
+		// 	await page.setContent(chapterContent);
+		// 	log('Content set successfully');
 
-		const pdfBuffer = await page.pdf({
-			format: 'A4',
-			margin: {
-				top: '20mm',
-				bottom: '20mm',
-				left: '20mm',
-				right: '20mm',
-			},
-			displayHeaderFooter: true,
-			headerTemplate:
-				'<div style="font-size: 10px; text-align: center; width: 100%; display: flex;">Downloaded using fanfic downloader</div>',
-			footerTemplate:
-				'<div style="font-size: 10px; text-align: center;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>',
-		});
+		// 	const pdfBuffer = await page.pdf({
+		// 		format: 'A4',
+		// 		margin: {
+		// 			top: '20mm',
+		// 			bottom: '20mm',
+		// 			left: '20mm',
+		// 			right: '20mm',
+		// 		},
+		// 		displayHeaderFooter: true,
+		// 		headerTemplate:
+		// 			'<div style="font-size: 10px; text-align: center; width: 100%; display: flex;">Downloaded using fanfic downloader</div>',
+		// 		footerTemplate:
+		// 			'<div style="font-size: 10px; text-align: center;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>',
+		// 	});
 
-		await browser.close();
-		log('PDF generated successfully');
+		// 	await browser.close();
+		// 	log('PDF generated successfully');
 
-		return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
+		// 	return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
 	} catch (err) {
 		log(`Error occurred: ${err}`);
 		return res.json({ error: err.toString() }, 500);
