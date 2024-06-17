1a48efa230c86fad68fd3eeba876f9946690a79f
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-06-18 02:21:10 +0530

    test func modified

--- test/multichapterDownload.test.mjs
+++ test/multichapterDownload.test.mjs
@@ -4,12 +4,13 @@ import fs from 'fs';
 async function generateAndSavePDF() {
 	try {
 		const response = await fetch(
-			'https://6661df0a82b533cdfb2e.appwrite.global/',
+			'https://667070bb2a3290c88cfb.appwrite.global/',
 			{
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify({
 					url: 'https://archiveofourown.org/works/56434537/chapters/143401636',
+					type: 'pdf',
 				}),
 			}
 		);
@@ -24,4 +25,29 @@ async function generateAndSavePDF() {
 	}
 }
 
-generateAndSavePDF();
+async function generateAndSaveEPUB() {
+	try {
+		const response = await fetch(
+			'https://667070bb2a3290c88cfb.appwrite.global/',
+			{
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					url: 'https://archiveofourown.org/works/56434537/chapters/143401636',
+					type: 'epub',
+				}),
+			}
+		);
+
+		const epubBuffer = await response.buffer();
+
+		fs.writeFileSync('generated-epub.epub', epubBuffer);
+
+		console.log('EPUB generated and saved successfully.');
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
+// generateAndSavePDF();
+generateAndSaveEPUB();
