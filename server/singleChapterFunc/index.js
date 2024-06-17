f219afae7c7e8998df6d160c37d018bf3e76050e
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-06-18 02:51:44 +0530

    feat: adding epub downloading for single chapter

--- server/singleChapterFunc/index.js
+++ server/singleChapterFunc/index.js
@@ -1,5 +1,11 @@
 import { execSync } from 'child_process';
 import puppeteer from 'puppeteer';
+import Epub from 'epub-gen';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+const readFile = promisify(fs.readFile);
 
 let installed = false;
 
@@ -11,11 +17,9 @@ async function getChapterContent(url, log) {
 	const page = await browser.newPage();
 
 	await page.setCacheEnabled(false);
-
 	await page.goto(url, { waitUntil: 'networkidle2' });
 
 	const chapterContent = await page.$eval('#workskin', (div) => div.innerHTML);
-
 	await browser.close();
 	return chapterContent;
 }
@@ -25,12 +29,17 @@ export default async ({ req, res, log }) => {
 		log('Function invoked');
 		log(`Request object: ${req}`);
 
-		const { url } = req.body;
+		const { url, type } = req.body;
 		log(`Parsed URL: ${url}`);
 
 		if (!url) {
 			log('No URL provided');
 			return res.json({ error: 'URL is required' }, 400);
+		}
+
+		if (!type) {
+			log('No type provided');
+			return res.json({ error: 'Type is required' }, 400);
 		}
 
 		// Install Chromium and dependencies if not installed
@@ -49,40 +58,63 @@ export default async ({ req, res, log }) => {
 		const chapterContent = await getChapterContent(url, log);
 		log('Content fetched successfully');
 
-		const browser = await puppeteer.launch({
-			executablePath: '/usr/bin/chromium-browser',
-			args: [
-				'--no-sandbox',
-				'--disable-setuid-sandbox',
-				'--disable-dev-shm-usage',
-			],
-		});
-		const page = await browser.newPage();
+		if (type === 'pdf') {
+			const browser = await puppeteer.launch({
+				executablePath: '/usr/bin/chromium-browser',
+				args: [
+					'--no-sandbox',
+					'--disable-setuid-sandbox',
+					'--disable-dev-shm-usage',
+				],
+			});
+			const page = await browser.newPage();
 
-		await page.setCacheEnabled(false);
+			await page.setCacheEnabled(false);
+			await page.setContent(chapterContent);
+			log('Content set successfully');
 
-		await page.setContent(chapterContent);
-		log('Content set successfully');
+			const pdfBuffer = await page.pdf({
+				format: 'A4',
+				margin: {
+					top: '20mm',
+					bottom: '20mm',
+					left: '20mm',
+					right: '20mm',
+				},
+				displayHeaderFooter: true,
+				headerTemplate:
+					'<div style="font-size: 10px; text-align: center; width: 100%; display: flex;">Downloaded using fanfic downloader</div>',
+				footerTemplate:
+					'<div style="font-size: 10px; text-align: center;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>',
+			});
 
-		const pdfBuffer = await page.pdf({
-			format: 'A4',
-			margin: {
-				top: '20mm',
-				bottom: '20mm',
-				left: '20mm',
-				right: '20mm',
-			},
-			displayHeaderFooter: true,
-			headerTemplate:
-				'<div style="font-size: 10px; text-align: center; width: 100%; display: flex;">Downloaded using fanfic downloader</div>',
-			footerTemplate:
-				'<div style="font-size: 10px; text-align: center;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>',
-		});
+			await browser.close();
+			log('PDF generated successfully');
 
-		await browser.close();
-		log('PDF generated successfully');
+			return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
+		} else if (type === 'epub') {
+			const epubOptions = {
+				title: 'Fanfic Story',
+				author: 'Unknown',
+				content: [
+					{
+						title: 'Story',
+						data: chapterContent,
+					},
+				],
+			};
 
-		return res.send(pdfBuffer, 200, { 'Content-Type': 'application/pdf' });
+			const outputPath = path.join('/tmp', 'story.epub');
+			await new Epub(epubOptions, outputPath).promise;
+
+			const epubBuffer = await readFile(outputPath);
+			log('EPUB generated successfully');
+
+			return res.send(epubBuffer, 200, { 'Content-Type': 'application/epub+zip' });
+		} else {
+			log('Unsupported file type requested');
+			return res.json({ error: 'Unsupported file type requested' }, 400);
+		}
 	} catch (err) {
 		log(`Error occurred: ${err}`);
 		return res.json({ error: err.toString() }, 500);
