bbe1a8d98a832314b825e2ef639ec00c031a37e1
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-06-19 12:08:02 +0530

    fix: links to be open in other tabs

--- frontend/src/main/Footer.tsx
+++ frontend/src/main/Footer.tsx
@@ -1,13 +1,22 @@
 const Footer = () => {
+	const handleGitHubClick = () => {
+		chrome.tabs.create({
+			url: 'https://github.com/AbhinavGupta-de/fanficDownloader',
+		});
+	};
+
+	const handleWebsiteClick = () => {
+		chrome.tabs.create({
+			url: '',
+		});
+	};
+
 	return (
 		<footer className="p-5 fixed bottom-0 flex gap-2 text-primary justify-center items-center text-[18px] w-full">
-			<a
-				href="https://github.com/AbhinavGupta-de/fanficDownloader"
-				className="underline"
-			>
+			<a href="" onClick={handleGitHubClick} className="underline">
 				Github
 			</a>
-			<a href="" className="underline">
+			<a href="" onClick={handleWebsiteClick} className="underline">
 				Help?
 			</a>
 		</footer>
